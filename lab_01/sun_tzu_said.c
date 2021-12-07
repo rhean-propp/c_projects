cd227d126569fc2fbdc494fc4e70082c0fef831d
Rhean Propp <43300080+rhean-propp@users.noreply.github.com>
2021-12-07 13:37:18 -0700

    Create sun_tzu_said.c

---
+++ lab_01/sun_tzu_said.c
@@ -0,0 +1,20 @@
+// Program calls a function and prints to terminal.
+
+#include<stdio.h>
+
+int sunTzuSaid();
+
+int main() {
+
+  sunTzuSaid();
+
+  return 0;
+}
+
+int sunTzuSaid() {
+
+  printf("Stay on topic.\n");
+  
+  return 0;
+
+}
